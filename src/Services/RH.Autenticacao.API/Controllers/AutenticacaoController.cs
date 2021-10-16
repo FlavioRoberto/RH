605def4056bb168b32823d8ecf27c3b0fcb61ad1
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-10-16 16:04:16 -0300

    Adicionando criação e autenticação de usuários básica

---
+++ src/Services/RH.Autenticacao.API/Controllers/AutenticacaoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RH.Autenticacao.API.Models;
+using System.Threading.Tasks;
+
+namespace RH.Autenticacao.API.Controllers
+{
+    [Route("api/autenticacao")]
+    public class AutenticacaoController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AutenticacaoController(SignInManager<IdentityUser> signInManager,
+                                      UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var usuario = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(usuario, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(usuario, false);
+                return Ok();
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Autenticar(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+
+            if (result.Succeeded)
+                return Ok();
+
+            return BadRequest();
+        }
+
+    }
+}
---
+++ src/Services/RH.Autenticacao.API/Models/UserViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RH.Autenticacao.API.Models
+{
+    public class UsuarioRegistro
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} não é um e-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string SenhaConfirmacao { get; set; }
+    }
+
+    public class UsuarioLogin
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} não é um e-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Senha { get; set; }
+    }
+
+}
